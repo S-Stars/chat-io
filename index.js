3d4530ca3fbe5aa73454cc0da852f22bd4e97d34
timinis <litimc3@gmail.com>
2019-12-28 12:27:40 -0800

    Add working server file

--- index.js
+++ index.js
@@ -2,18 +2,35 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const users = []
+const users = [];
+const chat = [];
 
-io.on('connection', function(socket){
-  socket.on('join',(userName)=> {
-    if(users.find(element=> element===userName)){
-      return socket.emit('userStatus', false)
+io.on('connection', function(socket) {
+  socket.on('join', userName => {
+    if (users.find(element => element === userName)) {
+      return socket.emit('userStatus', false);
     }
     users.push(userName);
-    return socket.emit('userStatus', true)
-  })
+    chat.push(`${userName} has joined the chat`);
+    io.sockets.emit('chat', chat);
+    return socket.emit('userStatus', true);
+  });
+  socket.on('grabChat', data => {
+    return socket.emit('chat', chat);
+  });
+  socket.on('leave', username => {
+    users.splice(users.indexOf(username));
+    console.log(username);
+    chat.push(`${username} has left the chat.`);
+    io.sockets.emit('chat', chat);
+  });
+  socket.on('message', message => {
+    chat.push(message);
+    //send to all client
+    io.sockets.emit('chat', chat);
+  });
 });
 
-http.listen(5000, function(){
+http.listen(5000, function() {
   console.log('listening on *:5000');
 });
